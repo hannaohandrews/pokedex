efa8a0148981df8d49adb75be9ad8fe42249e4ce
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-07 13:07:47 -0700

    making grids

--- pokedex/src/components/BaseStats.jsx
+++ pokedex/src/components/BaseStats.jsx
@@ -15,19 +15,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(hp, attack, defense, special_attack, special_defense,speed) {
+  return { hp, attack, defense, special_attack, special_defense,speed};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(159, 6.0, 24, 4.0,1,1,1),
+  
 ];
 
-export default function DenseTable() {
+export default function DenseTable(props) {
   const classes = useStyles();
 
   return (
@@ -47,14 +44,17 @@ export default function DenseTable() {
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    
+              <TableCell align="right">{row.hp}</TableCell>
+              <TableCell align="right">{row.attack}</TableCell>
+              <TableCell align="right">{row.defense}</TableCell>
+              <TableCell align="right">{row.special_attack}</TableCell>
+              <TableCell align="right">{row.special_defense}</TableCell>
+              <TableCell align="right">{row.speed}</TableCell>
+
             </TableRow>
+
+            
           ))}
         </TableBody>
       </Table>
