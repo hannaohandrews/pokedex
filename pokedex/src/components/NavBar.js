72066c5379dba1cec0e2252a0f20612c49a18b67
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-07 10:15:31 -0700

    adding images to navbar

--- pokedex/src/components/NavBar.js
+++ pokedex/src/components/NavBar.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import ImgComp from './ImgComp'
+import i1 from "../images/1.png";
+import i2 from "../images/2.png";
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <ImgComp src={i1} />
+          <ImgComp src={i2} />
           <a href="" className="Pokedex">
             Pokedex
           </a>
