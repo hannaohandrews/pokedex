d040aa05843dc9454c6db52d13f3db7195949efb
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-08 14:22:24 -0700

    Updates

---
+++ precache-manifest.284e7ebe2e781585f7f33ac1f5618f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff922bb6078132c68d0e82646f9fb8b",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "229d3a66b4d516261476",
+    "url": "/pokedex/static/css/main.535eca24.chunk.css"
+  },
+  {
+    "revision": "f0faf2dcd3f742f8ca5f",
+    "url": "/pokedex/static/js/2.cca817e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.cca817e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229d3a66b4d516261476",
+    "url": "/pokedex/static/js/main.a994c5f9.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "24c0b6c61d0241a0f9bf1b0cdb24ebfe",
+    "url": "/pokedex/static/media/1.24c0b6c6.png"
+  }
+]);
