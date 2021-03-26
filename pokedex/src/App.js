65b858e938783023529c0fd67adbbb6698bf22a6
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2021-03-26 08:21:24 -0700

    Update App.js

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import NavBar from "./components/NavBar.jsx";
 import Button from "@material-ui/core/Button";
